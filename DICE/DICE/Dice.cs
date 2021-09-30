7f180b67beb2edf3014f8114ddacfaccaaad4421
phanthianhthu <thuptagcc19106@fpt.edu.vn>
2021-09-30 19:05:38 +0700

    add 3 function class

--- DICE/DICE/Dice.cs
+++ DICE/DICE/Dice.cs
@@ -8,5 +8,9 @@ namespace DICE
 {
     class Dice
     {
+        public int Side { get; set; }
+
+        public Dice() { this.Side = 6; }
+        public Dice(int side) { this.Side = side; }
     }
 }
